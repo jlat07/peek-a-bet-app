ff4d24447511123f66273321ad7eb9beba1b6270
JLAT <jlat0128@gmail.com>
2023-11-01 03:57:57 +0000

    adding to

--- Peek-A-Bet.py
+++ Peek-A-Bet.py
@@ -8,6 +8,15 @@ ticket_manager = TicketManager()
 
 st.title("Peek-A-Bet")
 
+\
+# This is a simplified mapping for demonstration. This should be enhanced based on real data.
+matchup_mapping = {
+    'Week 1': {'Team A': 'Team B', 'Team C': 'Team D'},
+    'Week 2': {'Team A': 'Team C', 'Team B': 'Team D'},
+    # ... add more weeks as needed
+}
+
+
 # Sample data (this should eventually come from your data source)
 weeks = ['Week 1', 'Week 2', 'Week 3']
 teams = ['Team A', 'Team B', 'Team C', 'Team D']
@@ -29,7 +38,8 @@ else:
 
 # Add button to finalize this match-up
 if st.button("Add Match-up"):
-    temp_matchups.append(f"{selected_team} vs ???")  # Needs more logic to get opponent
+    opponent = matchup_mapping[selected_week].get(selected_team, "Unknown")
+    temp_matchups.append(f"{selected_team} vs {opponent}")
     temp_bets.append({
         'type': selected_bet_type,
         'value': selected_spread if selected_bet_type == 'Spread' else over_under_value
@@ -45,8 +55,48 @@ if st.button("Finalize Ticket"):
     ticket_manager.add_ticket(temp_matchups, temp_bets)
     temp_matchups.clear()
     temp_bets.clear()
+# Peek-A-Bet.py
 
-# Display all tickets
-st.subheader("All Tickets:")
-for ticket in ticket_manager.get_all_tickets().values():
-    st.write(ticket.display())
+#... [existing code]
+
+with st.beta_container():
+    col1, col2, col3 = st.beta_columns(3)
+    
+    with col1:
+        st.subheader("Ticket ID")
+        for ticket_id in ticket_manager.get_all_tickets():
+            st.write(ticket_id)
+
+    with col2:
+        st.subheader("Ticket Details")
+        for ticket in ticket_manager.ordered_tickets():
+            st.markdown(f"**Matchups:** {', '.join(ticket.matchups)}")
+            st.markdown(f"**Bets:** {', '.join([f'{bet["type"]} {bet["value"]}' for bet in ticket.bets])}")
+
+    with col3:
+        st.subheader("Organize")
+        for ticket_id in ticket_manager.get_all_tickets():
+            new_position = st.selectbox(f"Position for {ticket_id}", list(range(1, len(ticket_manager.get_all_tickets())+1)), index=ticket_manager.ticket_order.index(ticket_id))
+            ticket_manager.change_ticket_order(ticket_id, new_position-1)
+
+
+
+
+
+
+
+# Peek-A-Bet.py
+
+#... [existing code]
+
+total_win = len(ticket_manager.get_all_tickets()) * 100
+st.write(f"Total potential win amount: ${total_win}")
+
+
+# Peek-A-Bet.py
+
+#... [existing code]
+
+selected_week = st.selectbox("Select a Week", list(matchup_mapping.keys()))
+teams_for_week = list(matchup_mapping[selected_week].keys())
+selected_team = st.selectbox("Select a Team", teams_for_week)
--- utils/ticket_manager.py
+++ utils/ticket_manager.py
@@ -7,21 +7,30 @@ class Ticket:
         self.bets = bets
 
     def display(self):
-        # Dummy method for now
-        return f"Ticket ID: {self.ticket_id}, Matchups: {self.matchups}, Bets: {self.bets}"
+        # Convert bets to a string representation
+        bet_str = ', '.join([f"{bet['type']} {bet['value']}" for bet in self.bets])
+        return f"Ticket ID: {self.ticket_id}\nMatchups: {', '.join(self.matchups)}\nBets: {bet_str}"
 
+
+# ticket_manager.py
 
 class TicketManager:
     def __init__(self):
         self.tickets = {}
+        self.next_ticket_id = 1
+        self.ticket_order = []  # To keep track of ticket order
 
     def add_ticket(self, matchups, bets):
-        ticket_id = len(self.tickets) + 1
-        self.tickets[ticket_id] = Ticket(ticket_id, matchups, bets)
+        ticket = Ticket(self.next_ticket_id, matchups, bets)
+        self.tickets[self.next_ticket_id] = ticket
+        self.ticket_order.append(self.next_ticket_id)
+        self.next_ticket_id += 1
 
-    def get_all_tickets(self):
-        return self.tickets
+    #... [other methods]
 
-    def delete_ticket(self, ticket_id):
-        if ticket_id in self.tickets:
-            del self.tickets[ticket_id]
+    def change_ticket_order(self, ticket_id, new_position):
+        self.ticket_order.remove(ticket_id)
+        self.ticket_order.insert(new_position, ticket_id)
+
+    def ordered_tickets(self):
+        return [self.tickets[ticket_id] for ticket_id in self.ticket_order]
