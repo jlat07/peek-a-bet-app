5b3a1bed695076d7023393e0d42f47f18abb77fc
JLAT <jlat0128@gmail.com>
2023-11-03 03:54:31 +0000

    testing sliders and input

--- utils/matchup_data.py
+++ utils/matchup_data.py
@@ -48,12 +48,5 @@ matchup_mapping = {
 
 
 weeks = list(matchup_mapping.keys())
-
-# Sort the matchups by the first team name alphabetically
-for week, data in matchup_mapping.items():
-    sorted_matchups = dict(sorted(data['teams'].items()))
-    data['teams'] = sorted_matchups
-
-# Then extract all unique team names and sort them
-teams = sorted(list({team for week in matchup_mapping.values() for team in week['teams'].keys()}))
+teams = list({team for week in matchup_mapping.values() for team in week['teams'].keys()})
 
